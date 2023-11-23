36eec9f9a9ef115777870f3e2e0fc353ab346dc8
Ian Gerolamo <iangerolamo@gmail.com>
2023-11-23 18:44:15 -0300

    Novas funcionalidades

--- src/main/java/com/ig/safemoney/controller/TransactionController.java
+++ src/main/java/com/ig/safemoney/controller/TransactionController.java
@@ -3,6 +3,7 @@ package com.ig.safemoney.controller;
 import com.ig.safemoney.model.Transaction;
 import com.ig.safemoney.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -24,5 +25,23 @@ public class TransactionController {
     @PostMapping
     public Transaction createTransaction(@RequestBody Transaction transaction) {
         return transactionService.createTransaction(transaction);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Transaction> updateTransaction(
+            @PathVariable Long id,
+            @RequestBody Transaction updatedTransaction) {
+        Transaction updatedTransactionResult = transactionService.updateTransaction(id, updatedTransaction);
+
+        if (updatedTransaction != null) {
+            return ResponseEntity.ok(updatedTransactionResult);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTransaction(@PathVariable Long id) {
+        transactionService.deleteTransaction(id);
     }
 }
--- src/main/java/com/ig/safemoney/service/TransactionService.java
+++ src/main/java/com/ig/safemoney/service/TransactionService.java
@@ -2,10 +2,12 @@ package com.ig.safemoney.service;
 
 import com.ig.safemoney.model.Transaction;
 import com.ig.safemoney.repository.TransactionRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TransactionService {
@@ -20,4 +22,18 @@ public class TransactionService {
     public Transaction createTransaction(Transaction transaction) {
         return transactionRepository.save(transaction);
     }
+
+    public Transaction updateTransaction(Long id, Transaction updatedTransaction) {
+        Optional<Transaction> optionalTransaction = transactionRepository.findById(id);
+
+        return optionalTransaction.map(transaction -> {
+            BeanUtils.copyProperties(updatedTransaction, transaction, "id");
+            return transactionRepository.save(transaction);
+        }).orElse(null);
+    }
+
+    public void deleteTransaction(Long id) {
+        transactionRepository.deleteById(id);
+    }
+
 }
